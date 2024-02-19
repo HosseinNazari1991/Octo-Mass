e583e3fcb252c73562e55a755e6ac822faa2bd27
Hossein Nazari <hossein.nazarii.1991@gmail.com>
2024-02-19 22:55:44 +0330

    Start point in Mass Generator

---
+++ OctoMass/OctoMassInfo.cs
@@ -0,0 +1,25 @@
+﻿using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace OctoMass
+{
+    public class OctoMassInfo : GH_AssemblyInfo
+    {
+        public override string Name => "OctoMass";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("8aaca344-651e-4387-95f7-ab149772feeb");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-#Octo-mass
-Octo mass is an academic plugin helps designers create different alternatives in early stages design.
